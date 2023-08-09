4b8776434b7a10b9eb47503e22132818c9327b91
Fred <fgaringo@ylopo.com>
2023-08-10 07:50:36 +0800

    added stripe webhook

--- src/api/webhooks/controllers/webhooks.ts
+++ src/api/webhooks/controllers/webhooks.ts
@@ -1,45 +1,36 @@
-/**
- * A set of functions called "actions" for `webhooks`
- */
 const stripe = require("stripe")(
   process.env.STRAPI_ADMIN_TEST_STRIPE_SECRET_KEY
 );
 const unparsed = require("koa-body/unparsed.js");
+const io = require("socket.io")(strapi.server.httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
-export default {
-  stripeWebhook: async (ctx, next) => {
+module.exports = {
+  stripeWebhook: async (ctx) => {
     try {
       const event = await stripe.webhooks.constructEvent(
         ctx.request.body[unparsed],
         ctx.request.headers['stripe-signature'],
         process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET
       );
-      
 
       if (event.type === 'checkout.session.completed') {
-        strapi.log.info(`stripe event: ${JSON.stringify(event)}`);
-        const clientEmail: string = event.data.object.customer_details.email;
-        strapi.log.info(`client email: ${clientEmail}`);
-
-        if (clientEmail) {
-          const io = require("socket.io")(strapi.server.httpServer, {
-            cors: {
-              // cors setup
-              origin: "*",
-              methods: ["GET", "POST"],
-              allowedHeaders: ["my-custom-header"],
-              credentials: true,
-            },
+        const clientEmail = event.data.object.customer_details.email;
+        if (clientEmail && event.data.object) {
+          io.emit("useCheckoutStripeSuccess", {
+            email: clientEmail,
+            message: "Stripe checkout payment successful",
+            data: event.data.object,
           });
-          io.on('connection', (socket) => {
-            socket.emit("checkoutSuccessful", {
-              email: clientEmail,
-              message: "Stripe checkout payment successful",
-              data: event.data
-            });
-          })
         }
       }
+
       ctx.send({ received: true }); // Send a response to Stripe
     } catch (error) {
       console.error('Stripe webhook error:', error);
